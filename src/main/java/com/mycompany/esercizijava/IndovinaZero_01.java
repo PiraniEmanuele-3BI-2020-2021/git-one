e22558dc18e3e488fe7ecb3689012c27008394a1
PiraniEmanuele-3BI-2020-2021 <73538662+PiraniEmanuele-3BI-2020-2021@users.noreply.github.com>
2020-12-02 12:26:04 +0100

    Update IndovinaZero_01.java

--- src/main/java/com/mycompany/esercizijava/IndovinaZero_01.java
+++ src/main/java/com/mycompany/esercizijava/IndovinaZero_01.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package com.mycompany.esercizijava;
-
+import java.util.Scanner;
 /**
  *
  * @author 39392
  */
 public class IndovinaZero_01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        do{
+            System.out.println("Inserisci il valore");
+            n = input.nextInt();
+        } while(n>0 && n<0);
     
+    }
 }
